3c8cd991e192b4838064fa37654d56c0993f4261
Аlex Kukhtin <alex.kukhtin@outlook.com>
2018-12-06 10:29:55 +0200

    demo2

--- Apps/Demo/Dashboard/dashboard.template.js
+++ Apps/Demo/Dashboard/dashboard.template.js
@@ -2,11 +2,11 @@
 
 
 const template = {
-    properties: {
-    },
-    delegates: {
-        drawChart
-    }
+	properties: {
+	},
+	delegates: {
+		drawChart
+	}
 };
 
 module.exports = template;
@@ -19,49 +19,49 @@ const marginY = 20;
 
 function drawChart(graphics) {
 
-    const chart = graphics.append('svg')
-        .attr('viewBox', `0 0 ${width} ${height}`);
+	const chart = graphics.append('svg')
+		.attr('viewBox', `0 0 ${width} ${height}`);
 
-    const sine = d3.range(0, 40)
-        .map(k => {
-            let x = .5 * k * Math.PI;
-            return [x, Math.sin(x / 4)];
-        });
+	const sine = d3.range(0, 40)
+		.map(k => {
+			let x = .5 * k * Math.PI;
+			return [x, Math.sin(x / 4)];
+		});
 
-    // scale functions
-    const x = d3.scaleLinear()
-        .range([marginX, width - marginX / 2])
-        .domain(d3.extent(sine, d => d[0]));
+	// scale functions
+	const x = d3.scaleLinear()
+		.range([marginX, width - marginX / 2])
+		.domain(d3.extent(sine, d => d[0]));
 
-    const y = d3.scaleLinear()
-        .range([height - marginY, marginY])
-        .domain([-1, 1]);
+	const y = d3.scaleLinear()
+		.range([height - marginY, marginY])
+		.domain([-1, 1]);
 
-    // line generator
-    const line = d3.line()
-        .x(d => x(d[0]))
-        .y(d => y(d[1]));
+	// line generator
+	const line = d3.line()
+		.x(d => x(d[0]))
+		.y(d => y(d[1]));
 
 
-    chart.append('g')
-        .append('path')
-        .datum(sine)
-        .attr('d', line.curve(d3.curveCardinal))
-        .attr('stroke', '#0d82f8')
-        .attr('stroke-width', 1)
-        .attr('fill', 'none');
+	chart.append('g')
+		.append('path')
+		.datum(sine)
+		.attr('d', line.curve(d3.curveCardinal))
+		.attr('stroke', '#0d82f8')
+		.attr('stroke-width', 1)
+		.attr('fill', 'none');
 
-    // Add the X Axis
-    chart.append("g")
-        .attr("class", "axis")
-        .attr("transform", `translate(0, ${height / 2})`)
-        .call(d3.axisBottom(x));
+	// Add the X Axis
+	chart.append("g")
+		.attr("class", "axis")
+		.attr("transform", `translate(0, ${height / 2})`)
+		.call(d3.axisBottom(x));
 
-    // Add the Y Axis
-    chart.append("g")
-        .attr("class", "axis")
-        .attr("transform", `translate(${marginX}, 0)`)
-        .call(d3.axisLeft(y).ticks(10));
+	// Add the Y Axis
+	chart.append("g")
+		.attr("class", "axis")
+		.attr("transform", `translate(${marginX}, 0)`)
+		.call(d3.axisLeft(y).ticks(10));
 }
 
 
